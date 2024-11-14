0f5d2af7602e69987c30b5298b3fbb0d24bbf812
max1373 <ghelmanmx@gmail.com>
2024-11-14 21:35:02 +0200

    A

---
+++ medical-consultation.js
@@ -0,0 +1,4 @@
+document.getElementById("consultationForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    document.getElementById("consultationResponse").style.display = "block";
+});

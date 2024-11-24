d06b86dbc78edf0c75c6d62d76ccc8f4610c4e82
max1373 <ghelmanmx@gmail.com>
2024-11-24 12:11:15 +0200

    a

--- account.js
+++ account.js
@@ -1,10 +1,9 @@
 document.getElementById('patientProfileForm').addEventListener('submit', function (event) {
     event.preventDefault();
     const fullName = document.getElementById('fullName').value;
-    const idNumber = document.getElementById('idNumber').value;
-    const birthdate = document.getElementById('birthdate').value;
     const email = document.getElementById('email').value;
+    const picture = document.getElementById('profilePicture').value ? "נבחרה תמונה" : "לא נבחרה תמונה";
 
-    alert(`פרופיל נוצר בהצלחה עבור ${fullName}.\nפרטי אימייל: ${email}`);
+    alert(`פרופיל נוצר בהצלחה!\nשם: ${fullName}\nאימייל: ${email}\nתמונה: ${picture}`);
     this.reset();
 });

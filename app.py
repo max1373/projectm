03d1e3a9eeb0783d942095aa2745d5cbabb720f2
max1373 <ghelmanmx@gmail.com>
2024-12-19 14:23:01 +0200

    a

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/first')
+def index():
+    return render_template('medical-consultation.html')
+
+@app.route('/second')
+def index():
+    return render_template('test-results.html')
+
+@app.route('/theird')
+def index():
+    return render_template('general-exams.html')
+
+@app.route('/forth')
+def index():
+    return render_template('account.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
